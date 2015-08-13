fff40807af03956145b2ca0a1b975c014dea1be7
Adam Ruzicka <aruzicka@redhat.com>
2015-08-13 14:49:23 +0200

    Support for retry and timeout

--- app/lib/actions/remote_execution/run_proxy_command.rb
+++ app/lib/actions/remote_execution/run_proxy_command.rb
@@ -5,8 +5,14 @@ module Actions
       include ::Dynflow::Action::Cancellable
 
       def plan(proxy, hostname, script, options = {})
-        options = { :effective_user => nil }.merge(options)
+        options = default_connection_options
+                  .merge({ :effective_user => nil })
+                  .merge(options)
         super(proxy, options.merge(:hostname => hostname, :script => script))
+      end
+
+      def run(event = nil)
+        with_connection_error_handling(event) { super(event) }
       end
 
       def proxy_action_name
@@ -14,8 +20,14 @@ module Actions
       end
 
       def on_data(data)
-        super(data)
-        error! _("Script execution failed") if failed_run?
+        if data[:result] == 'initialization_error'
+          handle_connection_exception(data[:exception_class]
+                                       .constantize
+                                       .new(data[:exception_message]))
+        else
+          super(data)
+          error! _("Script execution failed") if failed_run?
+        end
       end
 
       def rescue_strategy
@@ -23,11 +35,40 @@ module Actions
       end
 
       def failed_run?
-        exit_status && proxy_output[:exit_status] != 0
+        event.data[:result] == 'initialization_error' ||
+          (exit_status && proxy_output[:exit_status] != 0)
       end
 
       def exit_status
         proxy_output && proxy_output[:exit_status]
+      end
+
+      private
+
+      def default_connection_options
+        { :connection_options => { :retry_interval => 15, :retry_count => 4 } }
+      end
+
+      def handle_connection_exception(exception, event = nil)
+        output[:failed_proxy_task_ids] ||= []
+        options = input[:connection_options]
+        if options[:retry_count] - output[:failed_proxy_task_ids].count > 0
+          output[:failed_proxy_task_ids] << output[:proxy_task_id]
+          output[:proxy_task_id] = nil
+          suspend do |suspended_action|
+            @world.clock.ping suspended_action,
+                              Time.now + options[:retry_interval],
+                              event
+          end
+        else
+          raise exception
+        end
+      end
+
+      def with_connection_error_handling(event = nil, &block)
+        block.call() if block_given?
+      rescue ::RestClient::Exception, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ETIMEDOUT => e
+        handle_connection_exception(e, event)
       end
     end
   end
